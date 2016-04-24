0dd180fa5bb048722f9260b5748524363d21b735
Kenji Rikitake <kenji.rikitake@acm.org>
2016-04-24 17:59:06 +0900

    avr-lcdtest.cpp: serial LCD display

--- avr-lcdtest.cpp
+++ avr-lcdtest.cpp
@@ -22,104 +22,53 @@
  * THE SOFTWARE.
  */
 
-#include "AVRTools/Analog2Digital.h"
+/* #define F_CPU (16000000UL) */
+
 #include "AVRTools/ArduinoPins.h"
 #include "AVRTools/InitSystem.h"
 #include "AVRTools/SystemClock.h"
 #include "AVRTools/I2cMaster.h"
 #include "AVRTools/I2cLcd.h"
-
-/* requires beaufort sensor shield */
-/* temperature macros */
-
-#define TC1 ((double)1.065) // 1.090/1023*1000
-#define LM60TEMP(x) ((((double)x) * TC1 - 424.0) / 6.25)
-#define ADTTEMP(x) (((double)((int16_t)x)) / (double)128.0)
-
-#define ADT7410_ADDRESS 0x48
-#define waiti2c() while(I2cMaster::busy()){}
+#include "AVRTools/USART0.h"
 
 I2cLcd lcd;
 char number[12];
 
-int16_t lm60temp(int16_t adcval) {
-    return (int16_t)(LM60TEMP(adcval) + 0.5);
-}
-
 int main() {
 
-    double adt0;
-    int16_t a0, a1, a2, a3;
-    uint8_t tempbytes[2];
-    uint16_t temp;
+    int c;
 
     initSystem();
     initSystemClock();
-    initA2D(kA2dReference11V);
     I2cMaster::start();
-
-    setGpioPinModeInput(pPinA00);
-    setGpioPinModeInput(pPinA01);
-    setGpioPinModeInput(pPinA02);
-    setGpioPinModeInput(pPinA03);
+    USART0::start(9600);
 
     setGpioPinModeOutput(pPin13);
     setGpioPinLow(pPin13);
-
-    // Set ADT7410 to 15-bit precision mode
-    I2cMaster::writeSync(ADT7410_ADDRESS, 0x03, 0x80);
 
     lcd.init();
     lcd.clear();
     lcd.home();
     lcd.autoscrollOff();
-    lcd.setBacklight(I2cLcd::kBacklight_Green);
-    lcd.displayTopRow("Waiting...");
-    delayMilliseconds(300);
-    lcd.clear();
     lcd.setBacklight(I2cLcd::kBacklight_White);
-    waiti2c();
-
     for(;;) {
-        for(uint8_t color = 0; color < 8 ; color++) {
-
-            if (0 == I2cMaster::readSync(ADT7410_ADDRESS, 0x00,
-                    sizeof(tempbytes), tempbytes)) {
-                temp = (tempbytes[0] << 8) | tempbytes[1];
+        if (-1 != (c = USART0::read())) {
+            if (c == 12) {
+                // CTRL/L: clear display
+                lcd.clear();
+                lcd.home();
+            } else if (c == 16) {
+                // CTRL/P: set upper row
+                lcd.setCursor(0, 0);
+            } else if (c == 14) {
+                // CTRL/N: set lower row
+                lcd.setCursor(1, 0);
+            } else if (c < 32) {
+                // do nothing for all the other control codes
+                // (CR and LF are also ignored)
             } else {
-                temp = 0x8000;
+                lcd.print((char)c);
             }
-            adt0 = ADTTEMP(temp);
-
-            a0 = lm60temp(
-                   readGpioPinAnalogV(makeGpioVarFromGpioPinAnalog(
-                          pPinA00)));
-            a1 = lm60temp(
-                   readGpioPinAnalogV(makeGpioVarFromGpioPinAnalog(
-                          pPinA01)));
-            a2 = lm60temp(
-                   readGpioPinAnalogV(makeGpioVarFromGpioPinAnalog(
-                          pPinA02)));
-            a3 = lm60temp(
-                   readGpioPinAnalogV(makeGpioVarFromGpioPinAnalog(
-                          pPinA03)));
-
-            lcd.setCursor(0, 0);
-            lcd.print("ADT: ");
-            lcd.print(adt0);
-
-            lcd.setCursor(1, 0);
-            lcd.print(a0);
-            lcd.print((char)0x20);
-            lcd.print(a1);
-            lcd.print((char)0x20);
-            lcd.print(a2);
-            lcd.print((char)0x20);
-            lcd.print(a3);
-            lcd.print((char)0x20);
-            writeGpioPinDigital(pPin13, color % 2);
-
-            delayMilliseconds(50);
         }
     }
 
