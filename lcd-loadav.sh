f40368f62ba1733857d855b5cc9a0685c7265393
Kenji Rikitake <kenji.rikitake@acm.org>
2016-06-03 01:04:33 +0900

    lcd-loadav.sh: change port name

--- lcd-loadav.sh
+++ lcd-loadav.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # CAUTION: ^L and ^N are actual control codes
-socat SYSTEM:"while true; do (sleep 5 && (uptime | sed 's/.*averages: //' | sed 's/^/Load average:/')); done" /dev/cu.usbmodem141121,rawer,clocal=1,ispeed=9600,ospeed=9600
+socat SYSTEM:"while true; do (sleep 5 && (uptime | sed 's/.*averages: //' | sed 's/^/Load average:/')); done" /dev/cu.usbmodem1A1221,rawer,clocal=1,ispeed=9600,ospeed=9600
